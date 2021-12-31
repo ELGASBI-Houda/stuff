16d68c942d3b175c93e2c8780bac072b7c577cef
HoudaElgasbi <73824947+Houda-123@users.noreply.github.com>
2021-12-31 13:39:56 +0100

    Update main.py

--- voice_assisstant/main.py
+++ voice_assisstant/main.py
@@ -47,5 +47,70 @@ def create_note():
                 filename = recognizer.recognize_google(audio)
                 filename = filename.lower()
 
+            with open(filename, 'w') as f:
+                f.write(note)
+                done = True
+                speaker.say(f"I succefully created the note {filename}")
+                speaker.runAndWait()
+
+        except speech_recognition.UnknownValueError:
+            recognizer = speech_recognition.Recognizer()
+            speaker.say("I did not understand you! Please try again!")
+            speaker.runAndWait()
+
+
+def add_todo():
+
+    global recognizer
+
+    speaker.say("What to do do you want me to add?")
+    speaker.runAndWait()
+
+    done = False 
+
+    while not done: 
+        try: 
+
+            with speech_recognition.Microphone() as mic:
+
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                item = recognizer.recognize_google(audio)
+                item = item.lower()
+
+                todo_list.append(item)
+                done = True 
+
+                speaker.say(f"I added {item} to the to do list!")
+                speaker.runAndWait()
+        
+        except speech_recognition.UnknownValueError:
+
+            recognizer = speech_recognition.Recognizer()
+            speaker.say("I did not understand you! Please try again!")
+            speaker.runAndWait()
+
+
+def show_todos():
+
+    speaker.say("The items on your to do list are the following")
+    for item in todo_list:
+        speaker.say(item)
+    speaker.runAndWait()
+
+
+def hello():
+    speaker.say("Hello. What can I do for you?")
+    speaker.runAndWait()
+
+
+def quit():
+    speaker.say("Bye")
+    speaker.runAndWait()
+    sys.exit(0)
+
+
+
 assistant = GenericAssisstant('intents.json') 
 assistant.train_model()
